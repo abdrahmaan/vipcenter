22c66ec78efb68f3cfefaa1c903d84fb3c282cbb
Abdulrahman <xmbedo@gmail.com>
2022-02-20 15:12:00 +0200

    New Studient Link API

--- js/screens/NewStu.js
+++ js/screens/NewStu.js
@@ -18,44 +18,65 @@ setInterval(() => {
 let inputs = document.querySelectorAll("input");
 let btnAdd = document.querySelector(".btn.btn-primary");
 let myAlert = document.querySelector(".alert.alert-danger");
-
+let mainUrl = "http://localhost:8090/VipCenter/config";
 
 // Focus 
 inputs[0].focus();
 
-let AddStudient = function () { 
-  
+let AddStudient = function (){
+
   let inputs = document.querySelectorAll("input,select");
-  console.log(inputs[1].value.length);
-  if (inputs[0] !== "" && inputs[0].value.length >= 9 && !inputs[0].value.includes("ى") && inputs[1].value.length == 11) {
-    console.log(inputs[0].value);
-    console.log(inputs[1].value);
-    console.log(inputs[2].value);
+
+  if (inputs[0] !== "" && inputs[0].value.length >= 9 && !inputs[0].value.includes("ى") && inputs[1].value.length == 11 && inputs[2].value !== "") {
+
+    let url = `${mainUrl}/new_stu.php?name=${inputs[0].value}&phone=${inputs[1].value}&saf=${inputs[2].value}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
 
 
-    console.log("done");
-    // Alert Setting
-    myAlert.textContent = "تم إضافة الطالب بنجاح"
-    myAlert.classList.contains("alert-danger") ? myAlert.classList.replace("alert-danger", "alert-success") : null;
-    myAlert.classList.contains("d-none") ? myAlert.classList.remove("d-none") : null;
-    setTimeout(() => {
-      myAlert.classList.add("d-none");
-    }, 2500);
-    // Inputs After Add Setting 
-    inputs[0].focus();
-    inputs[0].value = ""
-    inputs[1].value = ""
+        console.log(data);
+
+        if (data.status == 1) {
+
+          console.log("done");
+
+          // Alert Setting
+          myAlert.textContent = "تم إضافة الطالب بنجاح"
+          myAlert.classList.contains("alert-danger") ? myAlert.classList.replace("alert-danger", "alert-success") : null;
+          myAlert.classList.contains("d-none") ? myAlert.classList.remove("d-none") : null;
+          setTimeout(() => {
+            myAlert.classList.add("d-none");
+          }, 2500);
+          // Inputs After Add Setting 
+          inputs[0].focus();
+          inputs[0].value = "";
+          inputs[1].value = "";
+          
+
+        }
+      })
+      .catch(error => {
+
+        console.log(error);
+        myAlert.classList.contains("d-none") ? myAlert.classList.remove("d-none") : null;
+        myAlert.classList.contains("alert-success") ? myAlert.classList.replace("alert-success", "alert-danger") : null;
+        myAlert.textContent = "يوجد خطأ بقواعد البيانات";
+        setTimeout(() => {
+          myAlert.classList.add("d-none");
+        }, 4000);
+
+      });
 
   } else {
-
     myAlert.classList.contains("d-none") ? myAlert.classList.remove("d-none") : null;
     myAlert.classList.contains("alert-success") ? myAlert.classList.replace("alert-success", "alert-danger") : null;
-    myAlert.textContent = "تأكد من كتابة الإسم به حرف الـ ي بالنقاط أو تأكد من رقم التيليفون";
+    myAlert.textContent = "الرجاء التأكد من كتابه إسم المدرس ثلاثى , حرف الـ ي مكتوب بالنقاط , رقم تيليفون صحيح";
     setTimeout(() => {
       myAlert.classList.add("d-none");
     }, 4000);
     console.log("please Check data");
-
   }
 }
 
